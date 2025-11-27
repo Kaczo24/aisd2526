76cc20fb2742e1b0a9601815a2297e56ef8b398c
Kaczo24 <55463087+Kaczo24@users.noreply.github.com>
2025-11-27 20:49:42 +0100

    vs formatting + pointers and deleting

--- lista 2/zadanie.cpp
+++ lista 2/zadanie.cpp
@@ -11,7 +11,7 @@ void quickSort(float arr[], int low, int high) {
 
         float pivot = arr[high];
         int i = (low - 1);
-    
+
         for (int j = low; j <= high - 1; j++) {
 
             if (arr[j] <= pivot) {
@@ -19,7 +19,7 @@ void quickSort(float arr[], int low, int high) {
                 swap(arr[i], arr[j]);
             }
         }
-        
+
         swap(arr[i + 1], arr[high]);
 
         quickSort(arr, low, i);
@@ -30,30 +30,31 @@ void quickSort(float arr[], int low, int high) {
 
 void quickSort3(float arr[], int low, int high) {
     if (low < high) {
-        
+
         int mid = high - 1;
-        
-        if(arr[mid] > arr[high])
+
+        if (arr[mid] > arr[high])
             swap(arr[mid], arr[high]);
-        
+
         float pivot1 = arr[mid];
         float pivot2 = arr[high];
-        
+
         int i = low;
         int j = low;
-    
+
         for (int n = low; n < high - 1; n++) {
             if (arr[n] <= pivot1) {
                 swap(arr[j], arr[n]);
                 swap(arr[i], arr[j]);
                 i++;
                 j++;
-            } else if(arr[n] <= pivot2) {
+            }
+            else if (arr[n] <= pivot2) {
                 swap(arr[j], arr[n]);
                 j++;
             }
         }
-        
+
         swap(arr[j], arr[mid]);
         swap(arr[i], arr[j]);
         j++;
@@ -74,12 +75,12 @@ void radixsort(int arr[], int n, int d)
 
 
     for (int ex = 1; m / ex > 0; ex *= d) {
-        int output[n];
-        int *count = new int[(2 * d) - 1] { 0 };
+        int* output = new int[n];
+        int* count = new int[(2 * d) - 1]{ 0 };
 
         for (int i = 0; i < n; i++)
             count[((arr[i] / ex) % d) + d - 1]++;
-    
+
         for (int i = 1; i < 2 * d - 1; i++)
             count[i] += count[i - 1];
 
@@ -90,110 +91,110 @@ void radixsort(int arr[], int n, int d)
 
         for (int i = 0; i < n; i++)
             arr[i] = output[i];
+
+        delete[] output;
+        delete[] count;
     }
 }
 
 template <typename T>
 class List {
-    private:
+private:
     struct Node {
-        Node *next = nullptr, *prev = nullptr;
+        Node* next = nullptr, * prev = nullptr;
         T key;
         Node(T _key) : key(_key) {}
-        Node(Node &other) : key(other.key), next(other.next), prev(other.prev) {}
-        Node(Node &&other) : key(other.key) {
+        Node(Node& other) : key(other.key), next(other.next), prev(other.prev) {}
+        Node(Node&& other) : key(other.key) {
             next = other.next;
             prev = other.prev;
             other.prev = other.next = nullptr;
         }
-        ~Node() {
-            delete next;
-            delete prev;
-        }
-        
+        ~Node() { delete next; }
     };
-    Node *start = nullptr, *head = nullptr;
-    
-    public:
+    Node* start = nullptr, * head = nullptr;
+
+public:
+    ~List() { delete start; }
     struct Iterum {
-        ~Iterum(){}
-        Node *current = nullptr;
-        T prev() { if(current != nullptr && current->prev != nullptr) return current->prev->key; return 0;}
-        operator T() const {if(current != nullptr) return current->key; return 0; }
+        ~Iterum() {}
+        Node* current = nullptr;
+        T prev() { if (current != nullptr && current->prev != nullptr) return current->prev->key; return 0; }
+        operator T() const { if (current != nullptr) return current->key; return 0; }
     };
-    
+
     int count = 0;
-    
+
     void push(T key) {
         count++;
-        if(head == nullptr)
+        if (head == nullptr)
             head = new Node(key);
         else {
             head->next = new Node(key);
             head->next->prev = head;
             head = head->next;
         }
-        if(start == nullptr)
+        if (start == nullptr)
             start = head;
     }
-    
+
     T& operator[](int n) {
-        if(n > count) return NULL;
+        if (n > count) return NULL;
         Node* current;
-        for(int m = 0; m < n; m++) 
+        for (int m = 0; m < n; m++)
             current = current->head;
         return current->key;
     }
-    
+
     void deleteIndex(int n) {
-        if(n > count) return;
+        if (n > count) return;
         Node* current;
-        for(int m = 0; m < n; m++) 
+        for (int m = 0; m < n; m++)
             current = current->head;
-        
-        if(current->next != nullptr)
+
+        if (current->next != nullptr)
             current->next->prev = current->prev;
         else
             head = current->prev;
-            
-        if(current->prev != nullptr)
+
+        if (current->prev != nullptr)
             current->prev->next = current->next;
         else
             start = current->next;
-            
+
         delete current;
         count--;
     }
-    
-    bool iterate(Iterum &value) {
-        if(value.current == nullptr) value.current = start;
+
+    bool iterate(Iterum& value) {
+        if (value.current == nullptr) value.current = start;
         else value.current = value.current->next;
         return value.current != nullptr;
     }
-    
+
     void insertionSort() {
-        if(count <= 1) return;
-        if(count == 2) {
-            if(start->key > head->key) swap(start->key, head->key);
+        if (count <= 1) return;
+        if (count == 2) {
+            if (start->key > head->key) swap(start->key, head->key);
             return;
         }
-        Node *currentHead = start->next;
-        
-        for(Node *currentHead = start->next; currentHead != nullptr; ) {
-            Node *nextHead = currentHead->next, *sortingHead = currentHead->prev;
-            
-            if(sortingHead->key > currentHead->key) {
-                
+        Node* currentHead = start->next;
+
+        for (Node* currentHead = start->next; currentHead != nullptr; ) {
+            Node* nextHead = currentHead->next, * sortingHead = currentHead->prev;
+
+            if (sortingHead->key > currentHead->key) {
+
                 currentHead->prev->next = currentHead->next;
-                if(currentHead->next != nullptr)
+                if (currentHead->next != nullptr)
                     currentHead->next->prev = currentHead->prev;
                 else
                     head = currentHead->prev;
-                
-                while(sortingHead != nullptr && sortingHead->key > currentHead->key)
+
+                while (sortingHead != nullptr && sortingHead->key > currentHead->key)
                     sortingHead = sortingHead->prev;
-                    
-                if(sortingHead == nullptr) {
+
+                if (sortingHead == nullptr) {
                     start->prev = currentHead;
                     currentHead->prev = nullptr;
                     currentHead->next = start;
@@ -202,7 +203,7 @@ class List {
                 else {
                     currentHead->prev = sortingHead;
                     currentHead->next = sortingHead->next;
-                    if(sortingHead->next != nullptr)
+                    if (sortingHead->next != nullptr)
                         sortingHead->next->prev = currentHead;
                     else
                         head = currentHead;
@@ -210,7 +211,7 @@ class List {
                 }
             }
             currentHead = nextHead;
-            
+
         }
     }
 };
@@ -218,24 +219,25 @@ class List {
 void bucketSort(float arr[], int n) {
     float max = arr[0], min = arr[0];
     for (int i = 0; i < n; i++) {
-        if(arr[i] > max) max = arr[i];
-        if(arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
     }
-    
-    List<float> b[n];
-    for (int i = 0; i < n; i++) 
+
+    List<float>* b = new List<float>[n];
+    for (int i = 0; i < n; i++)
         b[(int)(n * (arr[i] - min) / (max - min + 1))].push(arr[i]);
-    
-    for (int i = 0; i < n; i++) 
+
+    for (int i = 0; i < n; i++)
         b[i].insertionSort();
-    
+
     int index = 0;
-    
+
     for (int i = 0; i < n; i++) {
         List<float>::Iterum val;
-        while(b[i].iterate(val))
+        while (b[i].iterate(val))
             arr[index++] = val;
     }
+    delete[] b;
 }
 
 int main()
@@ -243,54 +245,63 @@ int main()
     srand(time(NULL));
     using std::chrono::high_resolution_clock;
     using std::chrono::duration_cast;
+    using std::chrono::nanoseconds;
     using std::chrono::microseconds;
+
+    int size = 250000, tests = 10;
+    int* arr = new int[size];
     
-	int size = 500000, tests = 10;
-    int arr[size];
-	for(int d = 10; d <= 24; d++) {
-	    
-    	float totalTime = 0;
-    	for(int t = 0; t < tests; t++) {
-        
-            for(int n = 0; n < size; n++)
+    for (int d = 2; d <= 24; d++) {
+
+        float totalTime = 0;
+        for (int t = 0; t < tests; t++) {
+
+            for (int n = 0; n < size; n++)
                 arr[n] = (rand() % (size * 10)) - (size * 5);
-            
+
             auto t1 = high_resolution_clock::now();
             radixsort(arr, size, d);
             auto t2 = high_resolution_clock::now();
-            
+
             totalTime += duration_cast<microseconds>(t2 - t1).count() / 1000.0;
-    	}
+        }
         cout << d << "\t" << totalTime / tests << endl;
-	}
-	
-	cout << "\n\n------------------\n\n";
-	
-	int sizes[] = {10, 50, 100, 1000, 10000, 50000, 100000};
-	for(int size : sizes) {
-	    
-	    float quickTime = 0, quick3Time = 0, bucketTime = 0;
-    	for(int t = 0; t < tests; t++) {
-        
-	        float arr1[size], arr2[size], arr3[size];
-	        for(int n = 0; n < size; n++)
-	            arr1[n] = arr2[n] = arr3[n] = ((rand() % (size * 100)) - (size * 50)) / 100.0;
-            
+    }
+
+    delete[] arr;
+
+    cout << "\n\n------------------\n\n";
+
+    int sizes[] = { 10, 50, 100, 1000, 10000, 50000, 100000, 500000 };
+    for (int size : sizes) {
+
+        float quickTime = 0, quick3Time = 0, bucketTime = 0;
+        float* arr1 = new float[size], * arr2 = new float[size], * arr3 = new float[size];
+
+        for (int t = 0; t < tests; t++) {
+
+            for (int n = 0; n < size; n++)
+                arr1[n] = arr2[n] = arr3[n] = ((rand() % (size * 100)) - (size * 50)) / 100.0;
+
             auto t1 = high_resolution_clock::now();
-            quickSort(arr1, 0, size-1);
+            quickSort(arr1, 0, size - 1);
             auto t2 = high_resolution_clock::now();
-            quickSort3(arr2, 0, size-1);
+            quickSort3(arr2, 0, size - 1);
             auto t3 = high_resolution_clock::now();
             bucketSort(arr3, size);
             auto t4 = high_resolution_clock::now();
-            
-            quickTime += duration_cast<microseconds>(t2 - t1).count() / 1000.0;
-            quick3Time += duration_cast<microseconds>(t3 - t2).count() / 1000.0;
-            bucketTime += duration_cast<microseconds>(t4 - t3).count() / 1000.0;
-    	}
-    	cout << size << ":\tquickSort " << quickTime / tests 
-    	     << "\tquickSort3 " << quick3Time / tests 
-    	     << "\tbucketSort " << bucketTime / tests << endl;
-	}
-  	return 0;
+
+            quickTime += duration_cast<nanoseconds>(t2 - t1).count() / 1000.0;
+            quick3Time += duration_cast<nanoseconds>(t3 - t2).count() / 1000.0;
+            bucketTime += duration_cast<nanoseconds>(t4 - t3).count() / 1000.0;
+        }
+        cout << size << ":\tquickSort " << quickTime / tests
+            << "\tquickSort3 " << quick3Time / tests
+            << "\tbucketSort " << bucketTime / tests << endl;
+
+        delete[] arr1;
+        delete[] arr2;
+        delete[] arr3;
+    }
+    return 0;
 }
